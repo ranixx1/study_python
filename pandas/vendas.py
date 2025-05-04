be3306973c4f7f2ea09062248b11045653a68e69
ranilton <ranilton.c@escolar.ifrn.edu.br>
2025-05-04 00:39:37 -0300

    separando

--- pandas/vendas.py
+++ pandas/vendas.py
@@ -11,12 +11,12 @@ vendas_df.loc[1]
 vendas_df.loc[1:3]
 
 #pegar linhas que correspondem a uma condição
-#vendas_norteshopping_df = vendas_df.loc[vendas_df['ID Loja'] == 'Norte Shopping']
-#print(vendas_norteshopping_df)
+vendas_norteshopping_df = vendas_df.loc[vendas_df['ID Loja'] == 'Norte Shopping']
+print(vendas_norteshopping_df)
 
 #pegar varias linhas e colunas usando o loc
-#vendas_norteshopping_df = vendas_df.loc[vendas_df['ID Loja'] == 'Norte Shopping',['ID Loja', 'Produto', 'Quantidade']]
-#print(vendas_norteshopping_df)
+vendas_norteshopping_df = vendas_df.loc[vendas_df['ID Loja'] == 'Norte Shopping',['ID Loja', 'Produto', 'Quantidade']]
+print(vendas_norteshopping_df)
 
 #pegar um valor especifico
-#print(vendas_df.loc[1,'Produto'])
+print(vendas_df.loc[1,'Produto'])
