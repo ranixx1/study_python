6a655bd49b57fb25aa5f22a9c2227ffe61c6e2ab
ranilton <ranilton.c@escolar.ifrn.edu.br>
2025-05-09 17:47:45 -0300

    q12

---
+++ codeforce1/cedulas.py
@@ -0,0 +1,37 @@
+valor = int(input())
+print(valor)
+
+nota = 100
+quantidade = valor // nota
+valor = valor % nota
+print(f"{quantidade} nota(s) de R$ {nota},00")
+
+nota = 50
+quantidade = valor // nota
+valor = valor % nota
+print(f"{quantidade} nota(s) de R$ {nota},00")
+
+nota = 20
+quantidade = valor // nota
+valor = valor % nota
+print(f"{quantidade} nota(s) de R$ {nota},00")
+
+nota = 10
+quantidade = valor // nota
+valor = valor % nota
+print(f"{quantidade} nota(s) de R$ {nota},00")
+
+nota = 5
+quantidade = valor // nota
+valor = valor % nota
+print(f"{quantidade} nota(s) de R$ {nota},00")
+
+nota = 2
+quantidade = valor // nota
+valor = valor % nota
+print(f"{quantidade} nota(s) de R$ {nota},00")
+
+nota = 1
+quantidade = valor // nota
+valor = valor % nota
+print(f"{quantidade} nota(s) de R$ {nota},00")
