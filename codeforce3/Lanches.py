606af14925ff88c42b7cabfde8089702b6750b2e
ranilton <ranilton.c@escolar.ifrn.edu.br>
2025-05-10 18:04:16 -0300

    lista3 QF

---
+++ codeforce3/Lanches.py
@@ -0,0 +1,14 @@
+#Q6
+codigo,quantidade= map(int,input().split())
+ 
+if codigo ==1 :
+    print(f"Total: R$ {4*quantidade:.2f}")
+if codigo == 2 :
+    print(f"Total: R$ {4.5*quantidade:.2f}")
+if codigo == 3 :
+    print(f"Total: R$ {5*quantidade:.2f}")
+if codigo == 4 :
+    print(f"Total: R$ {2*quantidade:.2f}")
+if codigo == 5 :
+    print(f"Total: R$ {1.5*quantidade:.2f}")
+ 
