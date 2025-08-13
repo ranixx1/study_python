8ba75a284fb8305b8970150ae4e78064d89e58d0
ranilton <ranilton.c@escolar.ifrn.edu.br>
2025-08-12 22:40:37 -0300

    feat: adiciona questão que verifica se a lista é alternada ou não

--- revisao.py
+++ revisao.py
@@ -1,20 +1,11 @@
+lista = list(map(int, input().split()))
+alternada = True
+for i in range(len(lista)-1):
+    if lista[i] % 2 == lista[i+1]%2:
+        alternada = False
+        break
 
-
-a,b,c,d = map(int,input().split())
-
-if a<0 or b<0 or c<0 or d<0:
-
-    maior = -999999999
-
-    if a<0 and a>maior:
-        maior = a
-    if b<0 and b>maior:
-        maior = b
-    if c<0 and c>maior:
-        maior = c
-    if d<0 and d>maior:
-        maior = d
-    print(maior)
-        
-        
-        
+if alternada:
+    print("alternada")
+else:
+    print("Não alternada")
