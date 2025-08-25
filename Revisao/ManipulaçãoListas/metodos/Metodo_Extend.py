5aff56eb4b9e99b7c7e0806733e8c61f8b36bb91
ranilton <ranilton.c@escolar.ifrn.edu.br>
2025-08-25 09:55:25 -0300

    feat: metodo extend

---
+++ Revisao/ManipulaçãoListas/metodos/Metodo_Extend.py
@@ -0,0 +1,10 @@
+linguaguens_front = ['REACT','FLUTTER','SWIFT']
+linguaguens_fullstack = ['PYTHON','JAVA','C#']
+
+frameworks = ['LARAVEL','DJANGO','ANGULAR','VITE','ASP.NET']
+
+linguaguens_fullstack.extend(linguaguens_front)
+
+conjunto = linguaguens_fullstack + linguaguens_front + frameworks
+print(linguaguens_fullstack)
+print(conjunto)
