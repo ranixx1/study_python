96edcb8fb3ad5553d7052994a730cfd5b0e0f823
ranilton <ranilton.c@escolar.ifrn.edu.br>
2025-08-25 09:55:49 -0300

    feat: metodo pop

---
+++ Revisao/ManipulaçãoListas/metodos/Metodo_Pop.py
@@ -0,0 +1,6 @@
+linguagens = ['PYTHON', 'JAVASCRIPT', 'KOTLIN', 'GOLANG', 'TYPESCRIPT', 'JAVA', 'C', 'C#', 'RUST','PHP']
+
+item = linguagens.pop(2)
+
+print(f"Item: {item}")
+print(f"Lista completa: {linguagens}")
