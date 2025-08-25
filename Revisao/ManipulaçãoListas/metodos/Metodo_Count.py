c7c149c9561b39780c02d3f27d11700aa4b087c2
ranilton <ranilton.c@escolar.ifrn.edu.br>
2025-08-25 09:55:19 -0300

    feat: metodo count

---
+++ Revisao/ManipulaçãoListas/metodos/Metodo_Count.py
@@ -0,0 +1,5 @@
+lista = ['IPHONE','SAMSUNG','XIAOMI','MOTOROLA','LG','GOOGLE', 'IPHONE']
+
+conta_elementos = lista.count('IPHONE')
+
+print(f"A quantidade de vezes que a palavra {lista[lista.index('IPHONE')]} se repete é {conta_elementos}")
