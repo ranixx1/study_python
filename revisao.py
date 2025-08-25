1a84abb039d1bd8392bfe1e5f3253e3720dfd702
ranilton <ranilton.c@escolar.ifrn.edu.br>
2025-08-25 12:51:28 -0300

    feat: adicionando testes ao gitignore

--- revisao.py
+++
@@ -1,7 +0,0 @@
-a = list(map(int, input().split()))
-lista_ordenada = sorted(a)
-for i in range(len(lista_ordenada)-1):
-    if lista_ordenada[i+1]-lista_ordenada[i] == lista_ordenada[i+2] - lista_ordenada[i+1]:
-        print("PA")
-    else:
-        print("NÃo é")
