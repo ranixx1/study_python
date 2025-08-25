8042582fbfad4ca77e641b31d1f1ebf988a1e2b1
ranilton <ranilton.c@escolar.ifrn.edu.br>
2025-08-25 09:55:09 -0300

    feat: metodo copy

---
+++ Revisao/ManipulaçãoListas/metodos/Metodo_Copy.py
@@ -0,0 +1,10 @@
+linguagens = ['PYTHON', 'JAVASCRIPT', 'KOTLIN', 'GOLANG', 'TYPESCRIPT', 'JAVA', 'C', 'C#', 'RUST','PHP']
+
+
+nova_lista = linguagens.copy()
+
+nova_lista.append('SWIFT')
+nova_lista.append('FLUTTER')
+
+print(f"LISTA: {linguagens}")
+print(f"Nova lista : {nova_lista}")
