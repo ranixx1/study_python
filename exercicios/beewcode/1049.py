e68e44e8e56a287ddfed9f4c1b1f711397f05aab
ranilton <ranilton.c@escolar.ifrn.edu.br>
2025-09-04 21:24:24 -0300

    feat: adiciona questão de diagrama

---
+++ exercicios/beewcode/1049.py
@@ -0,0 +1,19 @@
+first = input("")
+second = input("")
+third = input("")
+if first == "vertebrado" and second =="mamifero" and third == "onivoro":
+    print("homem")
+elif first == "vertebrado" and second =="ave" and third == "carnivoro":
+    print("aguia")
+elif first == "vertebrado" and second =="ave" and third == "onivoro":
+    print("pomba")   
+elif first == "vertebrado" and second =="mamifero" and third == "herbivoro":
+    print("vaca")
+elif first == "invertebrado" and second =="inseto" and third == "hematofago":
+    print("pulga")
+elif first == "invertebrado" and second =="inseto" and third == "herbivoro":
+    print("lagarta")
+elif first == "invertebrado" and second =="anelideo" and third == "hematofago":
+    print("sanguessuga")
+elif first == "invertebrado" and second =="anelideo" and third == "onivoro":
+    print("minhoca")
